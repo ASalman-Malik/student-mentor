dab6daf9b617fa10c7b03b3445cae72b3856dfb5
Ahmad Salman Malik <malik.salman96@gmail.com>
2021-11-05 14:07:40 +0530

    commit

--- index.js
+++ index.js
@@ -1,99 +1,98 @@
-import express, { request, response } from 'express';
-import { MongoClient, ObjectId } from 'mongodb';
-import dotenv from 'dotenv';
+import express, { request, response } from "express";
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
 dotenv.config();
 const app = express();
+app.use(express.json());
 const port = process.env.PORT;
 const MONGO_DB = process.env.MONGODB;
 
-
-export async function createDBConnection(){
-    const client = new MongoClient(MONGO_DB);
-    await client.connect();
-    console.log("Coonected to MONGO DB ATLAS");
-    return client;
-} 
+async function createDBConnection() {
+  const client = new MongoClient(MONGO_DB);
+  await client.connect();
+  console.log("Coonected to MONGO DB ATLAS");
+  return client;
+}
 
 app.get("/", (request, response) => {
-    response.send("Welcome here we will be establishing connection with MONGODB ATLAS with Student-Mentor-API-DB");
-})
+  response.send(
+    "Welcome here we will be establishing connection with MONGODB ATLAS with Student-Mentor-API-DB"
+  );
+});
 
-
-app.post("/add-student", async(request, response) => {
-    const studentData= request.body;
-    console.log(studentData);
-    const client = await createDBConnection();
-    const result = await client
+app.post("/add-student", async (request, response) => {
+  const studentData = request.body;
+  console.log(studentData);
+  const client = await createDBConnection();
+  const result = await client
     .db("student-mentor")
     .collection("student")
-    .insertOne(request.body);
-    response.send(result);
-})
+    .insertMany(request.body);
+  response.send(result);
+});
 
-app.get("/get-students", async(request, response) => {
-    const client = await createDBConnection();
-    const result = await client
+app.get("/get-students", async (request, response) => {
+  const client = await createDBConnection();
+  const result = await client
     .db("student-mentor")
     .collection("student")
     .find({})
     .toArray();
-    response.send(result);
-})
+  response.send(result);
+});
 
-app.post("/add-mentor", async(request, response) => {
-    const mentorData = request.body;
-    console.log(mentorData);
-    const client = await createDBConnection();
-    const result = await client
+app.post("/add-mentor", async (request, response) => {
+  const mentorData = request.body;
+  console.log(mentorData);
+  const client = await createDBConnection();
+  const result = await client
     .db("student-mentor")
     .collection("mentor")
     .insertOne(request.body);
-    response.send(result);
-})
-app.get("/get-mentors", async(request, response) => {
-    const client = await createDBConnection();
-    const result = await client
+  response.send(result);
+});
+app.get("/get-mentors", async (request, response) => {
+  const client = await createDBConnection();
+  const result = await client
     .db("student-mentor")
     .collection("student")
     .find({})
     .toArray();
-    response.send(result);
-})
+  response.send(result);
+});
 
-app.get("/student-with-no-mentor", async(request, response) => {
-    const client = await createDBConnection();
-    const result = await client
+app.get("/student-with-no-mentor", async (request, response) => {
+  const client = await createDBConnection();
+  const result = await client
     .db("student-mentor")
     .collection("student")
-    .find({mentor: ""})
+    .find({ mentor: "" })
     .toArray();
-    response.send(result);
-})
+  response.send(result);
+});
 
-
-app.put("/one-mentor-many-student-assign", async(request, response) =>{
-    const mentorData = request.body;
-    const data = mentorData.data;
-    const nmentor = mentorData.mentor;
-    const omdata = data.map((id) => ObjectId(id));
-    const client = await createDBConnection();
-    const result = await client
+app.put("/one-mentor-many-student-assign", async (request, response) => {
+  const mentorData = request.body;
+  const data = mentorData.data;
+  const nmentor = mentorData.mentor;
+  const omdata = data.map((id) => ObjectId(id));
+  const client = await createDBConnection();
+  const result = await client
     .db("student-mentor")
     .collection("student")
-    .updateMany({_id : {$in : omdata }}, {$set : { mentor : nmentor}})
-    response.send(result);
-})
+    .updateMany({ _id: { $in: omdata } }, { $set: { mentor: nmentor } });
+  response.send(result);
+});
 
-app.get("/students-for-mentor", async(request, response) => {
-    const mentorData = request.body;
-    const client = await createDBConnection();
-    const result = await client
+app.get("/students-for-mentor", async (request, response) => {
+  const mentorData = request.body;
+  const client = await createDBConnection();
+  const result = await client
     .db("student-mentor")
     .collection("student")
-    .find({mentor : mentorData})
+    .find({ mentor: mentorData })
     .toArray();
-    response.send(result);
-})
-
+  response.send(result);
+});
 
 app.listen(port, () => console.log("The server is started at : ", port));
